28c9a3803c6dd498972fb11209e20a14222104ca
bramgeven <b.w.m.geven@student.tue.nl>
2023-09-08 11:01:52 +0200

    Applied black reformatting 08-09-23 1100

--- cycle_selection.py
+++ cycle_selection.py
@@ -3,8 +3,9 @@ import os
 import numpy as np
 from general_utilities import convert_image
 
+
 def calculate_cnr(roi, background):
-    """ Function to calculate the contrast-to-noise ratio (CNR) of a region of interest (ROI) and background, 
+    """Function to calculate the contrast-to-noise ratio (CNR) of a region of interest (ROI) and background,
     according to the ratio between the mean difference and variance in signal intensities.
 
     Args:
@@ -14,17 +15,18 @@ def calculate_cnr(roi, background):
     Returns:
         cnr (float): Contrast-to-noise ratio (CNR) of the ROI and background.
     """
-    # Exclude the pixels that are outside the imaging plane, only to be applied to the background. 
+    # Exclude the pixels that are outside the imaging plane, only to be applied to the background.
     background = background[background != 0]
-    
+
     diff = abs(np.mean(roi) - np.mean(background))
-    var = np.sqrt(np.std(roi)**2 + np.std(background)**2)
+    var = np.sqrt(np.std(roi) ** 2 + np.std(background) ** 2)
     cnr = diff / var
 
     return cnr
 
+
 def create_mask(echo_image, seg, desired_labels):
-    """ Function to create a mask of the US image with the desired labels.
+    """Function to create a mask of the US image with the desired labels.
 
     Args:
         echo_image (numpy array): echo image.
@@ -35,19 +37,22 @@ def create_mask(echo_image, seg, desired_labels):
         masked_image (numpy array): Masked echo image.
     """
     mask = np.zeros(echo_image.shape, dtype=np.uint8)
-    
+
     # Define mask with desired_labels.
     for label in desired_labels:
         mask[seg == label] = 255
 
-    # Give all pixels within mask same values as image, otherwise exclude them. 
+    # Give all pixels within mask same values as image, otherwise exclude them.
     masked_image = np.where(mask, echo_image, 300)
     masked_image = masked_image[masked_image != 300]
-    
+
     return masked_image
 
-def get_cnr_all_frames(path_to_images, path_to_segmentations, images_of_one_view, frames_to_exclude):
-    """ Function to calculate the contrast-to-noise ratio (CNR) of all frames in the image sequence.
+
+def get_cnr_all_frames(
+    path_to_images, path_to_segmentations, images_of_one_view, frames_to_exclude
+):
+    """Function to calculate the contrast-to-noise ratio (CNR) of all frames in the image sequence.
 
     Args:
         path_to_images (str): Path to the directory containing the echo image frames.
@@ -59,63 +64,71 @@ def get_cnr_all_frames(path_to_images, path_to_segmentations, images_of_one_view
         cnr_frames (list): List of CNR values for all frames in the image sequence.
     """
     cnr_frames = []
-    
+
     for frame_nr, name in enumerate(images_of_one_view):
         if frame_nr not in frames_to_exclude:
             # Define file location and load echo image frame
-            file_location_image = os.path.join(path_to_images, (name[:-7] + '_0000' + name[-7:]))
+            file_location_image = os.path.join(
+                path_to_images, (name[:-7] + "_0000" + name[-7:])
+            )
             echo_image = convert_image(file_location_image)
-            
-            # Define file location and load segmentation. 
+
+            # Define file location and load segmentation.
             file_location_seg = os.path.join(path_to_segmentations, name)
             seg = convert_image(file_location_seg)
-            
+
             # Create masks for ROI and background.
-            mask_roi = create_mask(echo_image, seg, [1,3]) # label specific
-            mask_background = create_mask(echo_image, seg, [2]) # label specific
-            
+            mask_roi = create_mask(echo_image, seg, [1, 3])  # label specific
+            mask_background = create_mask(echo_image, seg, [2])  # label specific
+
             # Calculate CNR and add to list.
             cnr = calculate_cnr(mask_roi, mask_background)
             cnr_frames.append(cnr)
-            
+
         else:
             cnr_frames.append(np.nan)
-    
+
     return cnr_frames
 
+
 def find_es_in_cycle(ES_points, ED_points_cycle, LV_areas):
-    """ Function to find the end-systolic (ES) point in the cardiac cycle.
+    """Function to find the end-systolic (ES) point in the cardiac cycle.
 
     Args:
         ES_points (list): List of end-systolic (ES) points in the image sequence.
         ED_points_cycle (list): List of end-diastolic (ED) points in the cardiac cycle.
         LV_areas (list): List of left ventricular (LV) areas in the image sequence.
-    
+
     Returns:
-        ES_point_cycle (list): List of end-systolic (ES) points in the cardiac cycle.    
+        ES_point_cycle (list): List of end-systolic (ES) points in the cardiac cycle.
     """
-    # Find all ES points between the ED points in cycle. 
-    ES_points_cycle = [ES_point for ES_point in ES_points if ES_point > ED_points_cycle[0] and ES_point < ED_points_cycle[1]]
-    
-    # If no ES point is defined in cycle, select one based on lowest LV area value within range. 
+    # Find all ES points between the ED points in cycle.
+    ES_points_cycle = [
+        ES_point
+        for ES_point in ES_points
+        if ES_point > ED_points_cycle[0] and ES_point < ED_points_cycle[1]
+    ]
+
+    # If no ES point is defined in cycle, select one based on lowest LV area value within range.
     if len(ES_points_cycle) == 0:
-        LV_areas_cycle = LV_areas[ED_points_cycle[0]:ED_points_cycle[1]]
+        LV_areas_cycle = LV_areas[ED_points_cycle[0] : ED_points_cycle[1]]
         min_LV_area = min([num for num in LV_areas_cycle if num != 0])
-        
+
         ES_point_cycle = [LV_areas.index(min_LV_area)]
-        
+
     # If more than 1 ES point is defined in cycle, select the point corresponding to the lowest LV area value within range.
     elif len(ES_points_cycle) > 1:
         min_area = min([LV_areas[i] for i in ES_points_cycle])
         ES_point_cycle = [i for i, area in enumerate(LV_areas) if area == min_area]
-        
+
     else:
         ES_point_cycle = ES_points_cycle
-    
+
     return ES_point_cycle
 
+
 def nr_outliers_in_cycle(outlier_frames, first_frame, last_frame):
-    """ Function to count the number of outliers in the cardiac cycle.
+    """Function to count the number of outliers in the cardiac cycle.
 
     Args:
         outlier_frames (list): List of outlier frames in the image sequence.
@@ -126,24 +139,32 @@ def nr_outliers_in_cycle(outlier_frames, first_frame, last_frame):
         nr_outliers (int): Number of outliers in the cardiac cycle.
     """
     nr_outliers = sum([first_frame < number < last_frame for number in outlier_frames])
-    
+
     return nr_outliers
+
 
 def score_criteria(my_list, method):
     my_list_rounded = [round(item, 1) for item in my_list]
-    
+
     min_list = min(my_list_rounded)
     max_list = max(my_list_rounded)
-    
-    if method == 'max all':
-        scores = [4 if item == max_list else 0 if item == min_list else 2 for item in my_list_rounded]
-    elif method == 'min all':
-        scores = [4 if item == min_list else 0 if item == max_list else 2 for item in my_list_rounded]
-                
+
+    if method == "max all":
+        scores = [
+            4 if item == max_list else 0 if item == min_list else 2
+            for item in my_list_rounded
+        ]
+    elif method == "min all":
+        scores = [
+            4 if item == min_list else 0 if item == max_list else 2
+            for item in my_list_rounded
+        ]
+
     return scores
 
+
 def get_best_cycle(cnr_cycles, nr_of_outliers_sf_qc, nr_of_outliers_mf_qc):
-    """ Function to select the most appropriate cardiac cycle.
+    """Function to select the most appropriate cardiac cycle.
 
     Args:
         cnr_cycles (list): List of CNR values for all cardiac cycles.
@@ -155,29 +176,40 @@ def get_best_cycle(cnr_cycles, nr_of_outliers_sf_qc, nr_of_outliers_mf_qc):
         score_cycle (int): Score of the most appropriate cardiac cycle.
     """
     # Calculate scores for each criterion.
-    scores_cnr = score_criteria(cnr_cycles, 'max all')
-    scores_sf_qc = score_criteria(nr_of_outliers_sf_qc, 'min all')
-    scores_mf_qc = score_criteria(nr_of_outliers_mf_qc, 'min all')
-    
-    # Calculate total score for each cycle.
-    scores_tot = [x + y + z for x, y, z in zip(scores_cnr, scores_sf_qc, scores_mf_qc)]    
+    scores_cnr = score_criteria(cnr_cycles, "max all")
+    scores_sf_qc = score_criteria(nr_of_outliers_sf_qc, "min all")
+    scores_mf_qc = score_criteria(nr_of_outliers_mf_qc, "min all")
 
-    # If more than 1 cycle has the highest score, add 1 to score of cycle with highest cnr. 
-    max_score = max(scores_tot)  
+    # Calculate total score for each cycle.
+    scores_tot = [x + y + z for x, y, z in zip(scores_cnr, scores_sf_qc, scores_mf_qc)]
+
+    # If more than 1 cycle has the highest score, add 1 to score of cycle with highest cnr.
+    max_score = max(scores_tot)
     if scores_tot.count(max_score) > 1:
-        idx_list = [idx for idx, score_val in enumerate(scores_tot) if score_val == max_score]
+        idx_list = [
+            idx for idx, score_val in enumerate(scores_tot) if score_val == max_score
+        ]
         idx_max_cnr = max(idx_list, key=lambda idx: cnr_cycles[idx])
         scores_tot[idx_max_cnr] += 1
-    
+
     # Select the cycle with the highest total score.
     cnr_best_cycle = cnr_cycles[scores_tot.index(max(scores_tot))]
-    
+
     score_cycle = max(scores_tot)
-     
+
     return cnr_best_cycle, score_cycle
 
-def get_main_cycle(cnr_frames, ed_points, es_points, lv_areas, frames_to_exclude_sf_qc, frames_to_exclude_lv_mf_qc, frames_to_exclude_la_mf_qc):
-    """ Function to select the most appropriate cardiac cycle.
+
+def get_main_cycle(
+    cnr_frames,
+    ed_points,
+    es_points,
+    lv_areas,
+    frames_to_exclude_sf_qc,
+    frames_to_exclude_lv_mf_qc,
+    frames_to_exclude_la_mf_qc,
+):
+    """Function to select the most appropriate cardiac cycle.
 
     Args:
         cnr_frames (list): List of CNR values for all frames in the image sequence.
@@ -194,35 +226,51 @@ def get_main_cycle(cnr_frames, ed_points, es_points, lv_areas, frames_to_exclude
         es_selected (list): List of end-systolic (ES) points in the most appropriate cardiac cycle.
         score (int): Score of the most appropriate cardiac cycle.
     """
-    cnr_cycles, ed_points_cycles, es_points_cycles, nr_outliers_sf_qc_cycles, nr_outliers_mf_qc_cycles = [], [], [], [], []
-        
+    (
+        cnr_cycles,
+        ed_points_cycles,
+        es_points_cycles,
+        nr_outliers_sf_qc_cycles,
+        nr_outliers_mf_qc_cycles,
+    ) = ([], [], [], [], [])
+
     for idx in range(len(ed_points) - 1):
         # Calculate average CNR of cycle.
-        cnr_all = np.nanmean(cnr_frames[ed_points[idx]:ed_points[idx+1]+1])
+        cnr_all = np.nanmean(cnr_frames[ed_points[idx] : ed_points[idx + 1] + 1])
         cnr_cycles.append(cnr_all)
-        
+
         # Find ED points in cycle.
-        ed_points_idx = [ed_points[idx], ed_points[idx+1]]
+        ed_points_idx = [ed_points[idx], ed_points[idx + 1]]
         ed_points_cycles.append(ed_points_idx)
 
         # Find ES point in cycle.
         es_point = find_es_in_cycle(es_points, ed_points_idx, lv_areas)
         es_points_cycles.append(es_point[0])
 
-        # Count number of outliers single-frame QC.   
-        count_outliers_sf_qc = nr_outliers_in_cycle(frames_to_exclude_sf_qc, ed_points[idx], ed_points[idx+1])     
+        # Count number of outliers single-frame QC.
+        count_outliers_sf_qc = nr_outliers_in_cycle(
+            frames_to_exclude_sf_qc, ed_points[idx], ed_points[idx + 1]
+        )
         nr_outliers_sf_qc_cycles.append(count_outliers_sf_qc)
-        
-        # Count number of outliers multi-frame QC. 
-        count_outliers_lv_mf_qc = nr_outliers_in_cycle(frames_to_exclude_lv_mf_qc, ed_points[idx], ed_points[idx+1])
-        count_outliers_la_mf_qc = nr_outliers_in_cycle(frames_to_exclude_la_mf_qc, ed_points[idx], ed_points[idx+1])
-        nr_outliers_mf_qc_cycles.append((count_outliers_lv_mf_qc + count_outliers_la_mf_qc))
-    
+
+        # Count number of outliers multi-frame QC.
+        count_outliers_lv_mf_qc = nr_outliers_in_cycle(
+            frames_to_exclude_lv_mf_qc, ed_points[idx], ed_points[idx + 1]
+        )
+        count_outliers_la_mf_qc = nr_outliers_in_cycle(
+            frames_to_exclude_la_mf_qc, ed_points[idx], ed_points[idx + 1]
+        )
+        nr_outliers_mf_qc_cycles.append(
+            (count_outliers_lv_mf_qc + count_outliers_la_mf_qc)
+        )
+
     # Get CNR of best cycle and corresponding ED and ES points.
-    cnr_best_cycle, score = get_best_cycle(cnr_cycles, nr_outliers_sf_qc_cycles, nr_outliers_mf_qc_cycles)
+    cnr_best_cycle, score = get_best_cycle(
+        cnr_cycles, nr_outliers_sf_qc_cycles, nr_outliers_mf_qc_cycles
+    )
     selection = cnr_cycles.index(cnr_best_cycle)
-    
-    ed_selected = ed_points_cycles[selection]      
+
+    ed_selected = ed_points_cycles[selection]
     es_selected = find_es_in_cycle(es_points_cycles, ed_selected, lv_areas)
-           
+
     return cnr_best_cycle, ed_selected, es_selected, score
--- general_utilities.py
+++ general_utilities.py
@@ -2,8 +2,9 @@ import cv2
 import numpy as np
 import SimpleITK as sitk
 
+
 def get_image_array(file_location):
-    """ Convert nifti or dicom file to 2D array.
+    """Convert nifti or dicom file to 2D array.
 
     Args:
         file_location (str): Location of the nifti or dicom file.
@@ -11,20 +12,24 @@ def get_image_array(file_location):
     Returns:
         image (np.ndarray): 2D array of the image.
     """
-    # Load image and convert to array. 
-    itk_image = sitk.ReadImage(file_location) 
+    # Load image and convert to array.
+    itk_image = sitk.ReadImage(file_location)
     image_array = sitk.GetArrayFromImage(itk_image)
-    
+
     # If array has a pseudo 3D shape, remove extra dimension (output nnU-Net v1).
     if len(image_array.shape) > 2:
-        image = np.reshape(image_array.transpose((1, 2, 0)), (image_array.shape[1], image_array.shape[2]))
+        image = np.reshape(
+            image_array.transpose((1, 2, 0)),
+            (image_array.shape[1], image_array.shape[2]),
+        )
     else:
         image = image_array
-        
+
     return image
 
+
 def separate_segmentation(seg):
-    """ Separate the LV, MYO and LA from the segmentation into separate segmentations.
+    """Separate the LV, MYO and LA from the segmentation into separate segmentations.
 
     Args:
         seg (np.ndarray): Segmentation of the image.
@@ -39,45 +44,53 @@ def separate_segmentation(seg):
     seg_1 = np.where(seg == 1, seg, 0)
     seg_2 = np.where(seg == 2, seg, 0)
     seg_3 = np.where(seg == 3, seg, 0)
-    
+
     return seg_0, seg_1, seg_2, seg_3
 
-def find_contours(seg, spec='all'):
-    """ Find the contours within the segmentation.
+
+def find_contours(seg, spec="all"):
+    """Find the contours within the segmentation.
 
     Args:
         seg (np.ndarray): Segmentation of the image.
         spec (str): Specification of the contours to find.
-    
+
     Returns:
         contours (list): List of contours.
     """
-    # Define the retrieval modes. 
+    # Define the retrieval modes.
     retrieval_modes = {
-        'all': cv2.RETR_LIST,
-        'external': cv2.RETR_EXTERNAL,
-        'tree': cv2.RETR_TREE}
-    
-    # Find contours based on the specified mode. 
-    contours, _ = cv2.findContours(seg, retrieval_modes.get(spec, cv2.RETR_LIST), cv2.CHAIN_APPROX_NONE)
-    
+        "all": cv2.RETR_LIST,
+        "external": cv2.RETR_EXTERNAL,
+        "tree": cv2.RETR_TREE,
+    }
+
+    # Find contours based on the specified mode.
+    contours, _ = cv2.findContours(
+        seg, retrieval_modes.get(spec, cv2.RETR_LIST), cv2.CHAIN_APPROX_NONE
+    )
+
     return contours
 
+
 def find_largest_contour(contours):
-    """ Find the largest contour within a list of contours, based on area.
+    """Find the largest contour within a list of contours, based on area.
 
     Args:
         contours (list): List of contours.
-    
+
     Returns:
         largest_contour (list): Largest contour.
-    """	
+    """
     largest_contour = max(contours, key=cv2.contourArea)
-    
+
     return largest_contour
 
-def combine_segmentations(segmentations, typeOfCombination='difference', labels=[1,2,3]):
-    """ Combine the segmentations of the LV, MYO and LA into one segmentation.	
+
+def combine_segmentations(
+    segmentations, typeOfCombination="difference", labels=[1, 2, 3]
+):
+    """Combine the segmentations of the LV, MYO and LA into one segmentation.
 
     A distinction can be made between the following types of combination:
     - difference: each structure gets other value.
@@ -88,48 +101,49 @@ def combine_segmentations(segmentations, typeOfCombination='difference', labels=
         segmentations (list): List of segmentations of the image.
         typeOfCombination (str): Type of combination of the segmentations.
         labels (list): List of labels of the segmentations.
-    
+
     Returns:
         total_seg (np.ndarray): Segmentation of the image with all structures.
-    """	
+    """
     # Give the first structure the value of the first label.
     if np.amax(segmentations[0]) == labels[0]:
         total_seg = segmentations[0].copy()
     else:
         total_seg = segmentations[0].copy() * labels[0]
-        
-    # Each structure gets other value.
-    if typeOfCombination == 'difference':
-        for nr, seg in enumerate(segmentations[1:]):
-            if seg.max() == labels[nr+1]:
-                total_seg += seg
-            else:
-                total_seg += seg * labels[nr+1]
 
-    # Each structure gets other value, but overlap is accounted for. 
-    if typeOfCombination == 'difference with overlap':
+    # Each structure gets other value.
+    if typeOfCombination == "difference":
         for nr, seg in enumerate(segmentations[1:]):
-            if seg.max() == labels[nr+1]:
+            if seg.max() == labels[nr + 1]:
                 total_seg += seg
             else:
-                total_seg += seg * labels[nr+1]
-                
-            if labels[nr+1] == 2:
-                total_seg[total_seg > labels[nr+1]] = labels[nr]
-            elif labels[nr+1] == 3:
-                total_seg[total_seg > labels[nr+1]] = labels[nr+1]
-    
+                total_seg += seg * labels[nr + 1]
+
+    # Each structure gets other value, but overlap is accounted for.
+    if typeOfCombination == "difference with overlap":
+        for nr, seg in enumerate(segmentations[1:]):
+            if seg.max() == labels[nr + 1]:
+                total_seg += seg
+            else:
+                total_seg += seg * labels[nr + 1]
+
+            if labels[nr + 1] == 2:
+                total_seg[total_seg > labels[nr + 1]] = labels[nr]
+            elif labels[nr + 1] == 3:
+                total_seg[total_seg > labels[nr + 1]] = labels[nr + 1]
+
     # All structures get same value.
-    elif typeOfCombination == 'no difference':
+    elif typeOfCombination == "no difference":
         for seg in segmentations[1:]:
             total_seg += seg
 
         total_seg[total_seg > 0] = 1
-        
+
     return total_seg
 
-def find_coordinates_of_holes(seg): 
-    """ Find the coordinates of the holes in a segmentation.
+
+def find_coordinates_of_holes(seg):
+    """Find the coordinates of the holes in a segmentation.
 
     Args:
         seg (np.ndarray): Segmentation of the image.
@@ -137,27 +151,37 @@ def find_coordinates_of_holes(seg):
     Returns:
         coordinates_holes (tuple): Coordinates of the holes in the segmentation.
     """
-    # Set all values larger than 0 to 1. 
+    # Set all values larger than 0 to 1.
     seg_same_val = seg.copy()
     seg_same_val[seg_same_val > 0] = 1
-    
-    # Find the contours of the structures in full segmentation. 
-    contours = find_contours(seg_same_val, 'external')
+
+    # Find the contours of the structures in full segmentation.
+    contours = find_contours(seg_same_val, "external")
 
     coordinates_holes_x_all, coordinates_holes_y_all = np.array([]), np.array([])
-        
-    for contour in contours:       
-        # Create a mask from the contour. 
+
+    for contour in contours:
+        # Create a mask from the contour.
         mask = cv2.drawContours(np.zeros_like(seg_same_val), [contour], 0, 255, -1)
-        
-        # Find the positions of all the zero pixels within the contour. 
+
+        # Find the positions of all the zero pixels within the contour.
         coordinates_holes_contour = np.where((mask == 255) & (seg_same_val == 0))
 
-        coordinates_holes_x, coordinates_holes_y = coordinates_holes_contour[0], coordinates_holes_contour[1]
-        
-        coordinates_holes_x_all = np.append(coordinates_holes_x_all, coordinates_holes_x) 
-        coordinates_holes_y_all = np.append(coordinates_holes_y_all, coordinates_holes_y) 
-    
-    coordinates_holes = (coordinates_holes_x_all.astype('int64'), coordinates_holes_y_all.astype('int64'))
-    
+        coordinates_holes_x, coordinates_holes_y = (
+            coordinates_holes_contour[0],
+            coordinates_holes_contour[1],
+        )
+
+        coordinates_holes_x_all = np.append(
+            coordinates_holes_x_all, coordinates_holes_x
+        )
+        coordinates_holes_y_all = np.append(
+            coordinates_holes_y_all, coordinates_holes_y
+        )
+
+    coordinates_holes = (
+        coordinates_holes_x_all.astype("int64"),
+        coordinates_holes_y_all.astype("int64"),
+    )
+
     return coordinates_holes
