1e56dc05fad06e2f4b406d9f3b24da2e3b5b28a8
bramgeven <b.w.m.geven@student.tue.nl>
2023-09-14 11:00:38 +0200

    Update 14-09-2023 1100
    - Finished multi-frame qc code

--- multi_frame_qc.py
+++ multi_frame_qc.py
@@ -1,67 +1,74 @@
-
+# This script contains functions to determine the quality control label for each view and for the patient as a whole.
 from collections import defaultdict
 
-def main_multi_frame_qc(all_files, views, cycle_information, multi_frame_qc_structural, multi_frame_qc_temporal):
+
+def count_values_in_range(list_values, start_value, end_value):
+    """Count the number of values in a list that fall within a specified range.
+
+    Args:
+        list_values (list): The list of values to search through.
+        start_value (int): The lower bound of the range.
+        end_value (int): The upper bound of the range.
+
+    Returns:
+        count (int): The number of values in the list that fall within the specified range.
+
+    Raises:
+        ValueError: If start_value is greater than end_value.
+    """
+    if start_value > end_value:
+        raise ValueError("start_value must be less than or equal to end_value")
+
+    count = sum(1 for value in list_values if start_value <= value <= end_value)
+
+    return count
 
 
+def main_multi_frame_qc(patient, views, cycle_information, multi_frame_qc_structural, multi_frame_qc_temporal, flagged_frame_threshold=2, dtw_thresholds=[1,2]):
+    """Main function to assign a quality control label to each view based on structural and temporal analysis.
+    The labels are combined to generate an overall patient-level label.
 
+    Args:
+        patient (str): The patient ID.
+        views (list): The list of views.
+        cycle_information (dict): The cycle information.
+        multi_frame_qc_structural (dict): The structural quality control information.
+        multi_frame_qc_temporal (dict): The temporal quality control information.
+        flagged_frame_threshold (int): The threshold for the number of flagged frames within a cycle (default: 2).
+        dtw_thresholds (list): The thresholds for the DTW distance between the area-time curve of a cycle and the atlas (default: [1,2]).
 
-    analysis_LV_2ch, analysis_LV_4ch, analysis_LA_2ch, analysis_LA_4ch = {}, {}, {}, {}
+    Returns:
+        analysis (dict): A dictionary containing analysis results.
+            - "label_per_view": A dictionary mapping views to their quality control labels.
+            - "label_combined": The combined quality control label for the patient.
+    
+    """
+    analysis = defaultdict(dict)
 
     for view in views:
+        # Load the begin and end points of the cycle.
+        ed_points = cycle_information["ed_points_selected"][view]
         
-        ED_points = cycle_information["ed_points_selected"][view]
-        ES_point = cycle_information["ES_point_selected"][view]
-          
-        QC2_outliers_LV = multi_frame_qc_structural["flagged_frames_lv"][view]
-        QC2_outliers_LA = multi_frame_qc_structural["flagged_frames_la"][view]
-                
-        dtw_LV = multi_frame_qc_temporal["dtw_lv"][view]
-        dtw_LA = multi_frame_qc_temporal["dtw_la"][view]
+        # Load the flagged frames within the cycle and count the number of flagged frames within the cycle.
+        flagged_frames_lv = multi_frame_qc_structural["flagged_frames_lv"][view]
+        flagged_frames_la = multi_frame_qc_structural["flagged_frames_la"][view]
         
-        score_LV, score_LA = 0, 0
-        
-        nr_outliers_QC2_LV_cycle = count_values_between(QC2_outliers_LV, ED_points[0], ED_points[1]+1)
-        nr_outliers_QC2_LA_cycle = count_values_between(QC2_outliers_LA, ED_points[0], ED_points[1]+1)
-    
-        # Outliers QC2
-        if nr_outliers_QC2_LV_cycle > 1:
-            score_LV += 1
+        nr_flagged_frames_lv_in_cycle = count_values_in_range(flagged_frames_lv, ed_points[0], ed_points[1])
+        nr_flagged_frames_la_in_cycle = count_values_in_range(flagged_frames_la, ed_points[0], ed_points[1])
 
-        if nr_outliers_QC2_LA_cycle > 1:
-            score_LA += 1
+        # Load the DTW distance between the area-time curve of a cycle and the atlas.
+        dtw_lv = multi_frame_qc_temporal["dtw_lv"][view]
+        dtw_la = multi_frame_qc_temporal["dtw_la"][view]
         
-        if dtw_LV > 1:
-            score_LV += 1
+        # Compute the score and label for the current view.
+        score_lv = int(nr_flagged_frames_lv_in_cycle >= flagged_frame_threshold) + int(dtw_lv > dtw_thresholds)
+        score_la = int(nr_flagged_frames_la_in_cycle >= flagged_frame_threshold) + int(dtw_la > dtw_thresholds)
 
-        if dtw_LA > 2:
-            score_LA += 1
-        
-        # True if flagged, False if unflagged
-        label_LV = True if score_LV >= 1 else False
-        label_LA = True if score_LA >= 1 else False
-        
-        if image.endswith('a2ch'):
-            analysis_LV_2ch[image] = label_LV
-            analysis_LA_2ch[image] = label_LA
-        elif image.endswith('a4ch'):
-            analysis_LV_4ch[image] = label_LV
-            analysis_LA_4ch[image] = label_LA
-            
-    analysis = {}
-    analysis_all = {}
-    analysis_LV_combined, analysis_LA_combined = {}, {}
+        # Save the score and label for the current view.
+        label_lv = score_lv >= 1
+        label_la = score_la >= 1
+        analysis["label_per_view"][view] = label_lv or label_la
+           
+    analysis["label_combined"][patient] = analysis["label_per_view"][views[0]] or analysis["label_per_view"][views[1]]
 
-    patients = sorted(set([i[:15] for i in all_files if i.startswith('cardiohance')]))
-    for patient in patients:
-        
-        images_of_one_view_unsorted = [image for image in images if image.startswith(patient)]
-        images_of_one_view = sorted(images_of_one_view_unsorted, key=lambda x: x[25:29])
-        
-        analysis_LV_combined[patient] = add_booleans(analysis_LV_2ch[images_of_one_view[0]], analysis_LV_4ch[images_of_one_view[1]])
-        analysis_LA_combined[patient] = add_booleans(analysis_LA_2ch[images_of_one_view[0]], analysis_LA_4ch[images_of_one_view[1]])
-        analysis_all[patient] = add_booleans(analysis_LV_combined[patient], analysis_LA_combined[patient])
-
-    analysis['ALL'] = analysis_all
-    analysis['LV'] = analysis_LV_combined
-    analysis['LA'] = analysis_LA_combined
+    return analysis
